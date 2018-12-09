a339f3bc79f57bef7e80128cff62459f313369d9
gregory propst <gregory.propst@gmail.com>
2018-12-08 19:43:09 -0500

    of cours ethat isn't a thing

--- server/main.lua
+++ server/main.lua
@@ -4,7 +4,8 @@ TriggerEvent('esx:getSharedObject', function(obj)
     ESX = obj
 end)
 
-ESX.RegisterServerEvent('blarglebus:finishRoute', function(source, amount)
+RegisterNetEvent('blarglebus:finishRoute')
+AddEventHandler('blarglebus:finishRoute', function(source, amount)
     local player = ESX.GetPlayerFromId(source)
     player.addMoney(amount)
 end)
