ce64c55c72e28e3db4640ddeac30f98a2746f501
gregory propst <gregory.propst@gmail.com>
2018-12-08 19:38:23 -0500

    whoopsies

--- locales/en.lua
+++ locales/en.lua
@@ -2,8 +2,8 @@ Locales['en'] = {
     ['drive_to_first_marker'] = 'Get in bus and drive to first stop.',
     ['drive_to_next_marker']  = 'Continue to next stop.',
     ['wait_for_passengers']   = 'Wait for passengers to finish loading and unloading',
-    ['return_to_terminal']    = 'Route is finished. Return to the terminal.'
-    
+    ['return_to_terminal']    = 'Route is finished. Return to the terminal.',
+
     ['airport_route']         = 'Airport Shuttle',
     ['start_airport_route']   = 'press ~INPUT_PICKUP~ to start airport route',
     ['intercity_route']       = 'Intercity Route',
--- server/main.lua
+++ server/main.lua
@@ -1,3 +1,9 @@
+ESX = nil
+
+TriggerEvent('esx:getSharedObject', function(obj)
+    ESX = obj
+end)
+
 ESX.RegisterServerEvent('blarglebus:finishRoute', function(source, amount)
     local player = ESX.GetPlayerFromId(source)
     player.addMoney(amount)
