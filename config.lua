d5c996da19115546a3593f3a2d5651cc8bdc4595
gregory propst <gregory.propst@gmail.com>
2018-12-11 19:01:12 -0500

    adjustments to intercity route

--- config.lua
+++ config.lua
@@ -35,21 +35,24 @@ IntercityRoute = {
     Name = 'intercity_route',
     BusModel = 'bus',
     Capacity = 15,
-    Doors = {1, 2},
+    Doors = {0, 1, 2, 3},
     FirstSeat = 1,
     SpawnPoint = {x = 427.6807, y = -582.7913, z = 28.5165, heading = 135.64},
     Payment = 15000,
     Stops = {
-        {x = 424.7632, y = -638.9176, z = 28.5001, heading = 'Dashound Bus Center', unloadType = Config.UnloadType.All},
-        -- TODO more western routes here
+        {x = 424.7632, y = -638.9176, z = 28.5001, name = 'Dashound Bus Center', unloadType = Config.UnloadType.All},
+        {x = -2973.2265, y = 408.1283, z = 15.1100, name = 'Banham Canyon', unloadType = Config.UnloadType.Some},
+        {x = -2522.2990, y = 2340.5200, z = 33.0598, name = 'Lago Zancudo', unloadType = Config.UnloadType.Some},
+        {x = -2285.6760, y = 4265.5454, z = 43.7155, name = 'North Chumash', unloadType = Config.UnloadType.Some},
+        {x = -1525.0812, y = 4949.1962, z = 61.9035, name = 'Chiliad Mtn State Park', unloadType = Config.UnloadType.Some},
         {x = -329.8910, y = 6185.0571, z = 31.6218, name = 'Paleto Bay', unloadType = Config.UnloadType.Some},
         {x = -17.2458, y = 6500.5517, z = 31.5728, name = 'Paleto Bay', unloadType = Config.UnloadType.Some},
         {x = 1660.6237, y = 4857.0190, z = 41.2123, name = 'Grapeseed', unloadType = Config.UnloadType.Some},
         {x = 1962.9892, y = 3710.2802, z = 32.2184, name = 'Sandy Shores', unloadType = Config.UnloadType.Some},
-        {x = 1063.4536, y = 2675.3242, z = 39.3417, name = 'Near Harmony', unloadType = Config.UnloadType.Some},
         {x = 2094.8310, y = 3566.1850, z = 41.8067, name = 'Senora National Park', unloadType = Config.UnloadType.Some},
         {x = 2237.2424, y = 3190.7673, z = 48.7102, name = 'Senora National Park', unloadType = Config.UnloadType.Some},
-        {x = 424.7632, y = -638.9176, z = 28.5001, heading = 'Dashound Bus Center', unloadType = Config.UnloadType.All},
+        {x = 549.9940, y = 2700.2866, z = 42.1508, name = 'Harmony', unloadType = Config.UnloadType.Some},
+        {x = 424.7632, y = -638.9176, z = 28.5001, name = 'Dashound Bus Center', unloadType = Config.UnloadType.All},
     }
 }
 
