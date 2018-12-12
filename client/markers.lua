8e28794e6fa117c28fac3ceac94b37eadaf503c7
gregory propst <gregory.propst@gmail.com>
2018-12-12 18:32:09 -0500

    switch marker type to not be a giant-ass circle

--- client/markers.lua
+++ client/markers.lua
@@ -26,7 +26,31 @@ end
 
 function drawCircle(coords)
     local markerSize = Config.Marker.Size
-    DrawMarker(1, coords.x, coords.y, coords.z, 0, 0, 0, 0, 0, 0, markerSize, markerSize, markerSize, 20, 200, 20, 100, 0, 0, 2, 0, 0, 0, 0)
+    DrawMarker(21,  -- type, MarkerTypeChevronUpx2
+        coords.x,   -- posX
+        coords.y,   -- posY
+        coords.z,   -- posZ
+        0,          -- dirX
+        0,          -- dirY
+        0,          -- dirZ
+        0,          -- rotX
+        0,          -- rotY
+        180,        -- rotZ
+        markerSize, -- scaleX
+        markerSize, -- scaleY
+        markerSize, -- scaleZ
+        20,         -- red
+        200,        -- green
+        20,         -- blue
+        100,        -- alpha
+        true,       -- bobUpAndDown
+        true,       -- faceCamera
+        2,          -- p19 "Typically set to 2. Does not seem to matter directly."
+        0,          -- rotate
+        0,          -- textureDict
+        0,          -- textureName
+        0           -- drawOnEnts
+    )
 end
 
 Markers = {
