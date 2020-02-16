ed596ce9a6f0c20a94096e5130f0b550fead7b16
Tinky <48660658+Tinky124@users.noreply.github.com>
2020-02-16 14:38:26 +0100

    Some Server Side checks :)

--- server/main.lua
+++ server/main.lua
@@ -4,7 +4,13 @@ TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
 RegisterNetEvent('blarglebus:finishRoute')
 AddEventHandler('blarglebus:finishRoute', function(amount)
+    local xPlayer = ESX.GetPlayerFromId(source)
+    
+    if xPlayer.job.name == 'busdriver' then
     ESX.GetPlayerFromId(source).addMoney(amount)
+        print(('blarglebus: %s attempted to exploit the job!'):format(xPlayer.identifier))
+		xPlayer.kick("You are kicked because of cheat!")
+	end
 end)
 
 RegisterNetEvent('blarglebus:passengersLoaded')
