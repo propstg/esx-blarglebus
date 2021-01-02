55717752e3c81cd95eb90c3fe367b1c5b379167f
EGUltraTM <31127586+EGUltraTM@users.noreply.github.com>
2021-01-02 23:47:00 +0100

    Add Scenic Bus to Route

---
+++ config/scenic.lua
@@ -0,0 +1,27 @@
+ScenicRoute = {
+    Name = 'scenic_route',
+    Bus = BusType.Scenic,
+    SpawnPoint = {x = 427.6807, y = -582.7913, z = 28.5165, heading = 135.64},
+    BusReturnPoint = {x = 427.6807, y = -582.7913, z = 28.5165, heading = 135.64},
+    Payment = 15000,
+    PaymentPerPassenger = 50,
+    Lines = {
+        {
+            Name = 'scenic_route',
+            BusColor = 39,
+            Stops = {
+                {x = 424.7632,   y = -638.9176, z = 28.5001, name = 'stop_dashound', unloadType = UnloadType.All},
+                {x = -2973.2265, y = 408.1283,  z = 15.1100, name = 'stop_banham_canyon', unloadType = UnloadType.Some},
+                {x = -2522.2990, y = 2340.5200, z = 33.0598, name = 'stop_lago_zancudo', unloadType = UnloadType.Some},
+                {x = -2285.6760, y = 4265.5454, z = 43.7155, name = 'stop_north_chumash', unloadType = UnloadType.Some},
+                {x = -1525.0812, y = 4949.1962, z = 61.9035, name = 'stop_chiliad', unloadType = UnloadType.Some},
+                {x = -329.8910,  y = 6185.0571, z = 31.6218, name = 'stop_paleto_bay', unloadType = UnloadType.Some},
+                {x = 1660.6237,  y = 4857.0190, z = 41.2123, name = 'stop_grapeseed', unloadType = UnloadType.Some},
+                {x = 1962.9892,  y = 3710.2802, z = 32.2184, name = 'stop_sandy_shores', unloadType = UnloadType.Some},
+                {x = 2237.2424,  y = 3190.7673, z = 48.7102, name = 'stop_senora_park', unloadType = UnloadType.Some},
+                {x = 549.9940,   y = 2700.2866, z = 42.1508, name = 'stop_harmony', unloadType = UnloadType.Some},
+                {x = 424.7632,   y = -638.9176, z = 28.5001, name = 'stop_dashound', unloadType = UnloadType.All},
+            }
+        }
+    }
+}
